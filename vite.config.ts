f79cf7f30b7cf9ff51c449aa8557b9cc43b2a37e
mariana4ads <mariana4ads@gmail.com>
2025-08-28 00:50:58 -0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://mariana4ads.github.io/card-tailwind/"
 });
